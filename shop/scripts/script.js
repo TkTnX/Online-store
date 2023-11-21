68cd107f3e080a3af1659989a3d86b3d5df9a4ad
TMR <timur20-09@mail.ru>
2023-11-21 18:48:08 +0500

    Online-store
    Добавлена возможность нажимать на кнопку "О нас"

--- shop/scripts/script.js
+++ shop/scripts/script.js
@@ -13,3 +13,37 @@ function addProduct() {
   productsCounter.innerHTML = i++;
   productsCounter.classList.add("added-products-counter-styles");
 }
+
+/* about-us-btn */
+/* open */
+document.getElementById("about-us-btn").addEventListener("click", function () {
+  document.getElementById("about-us-modal-id").classList.add("open");
+});
+
+/* //open */
+
+/* close */
+document
+  .getElementById("about-us-modal-close-id")
+  .addEventListener("click", function () {
+    document.getElementById("about-us-modal-id").classList.remove("open");
+  });
+
+/* // close */
+
+/* Закрытие вне окна */
+
+document
+  .querySelector("#about-us-modal-id .about-us-modal-txt")
+  .addEventListener("click", (event) => {
+    event._isClickWithInModal = true;
+  });
+
+document
+  .getElementById("about-us-modal-id")
+  .addEventListener("click", (event) => {
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove("open");
+  });
+
+/*  // Закрытие вне окна */
